2631cfe501a9afedf2c27bfcf823c378bb96ed46
DeluxeCS <943388027@qq.com>
2020-09-23 16:37:43 +0800

    9-22

--- src/main.js
+++ src/main.js
@@ -1,8 +1,29 @@
 import Vue from "vue";
+import axios from "axios";
+import VueAxios from 'vue-axios';
 import App from "./App.vue";
 import router from "./router/index";
 
 Vue.config.productionTip = false;
+
+Vue.use(VueAxios,axios);
+// 根据前端跨域方式修改
+axios.defaults.baseURL = "/api";
+axios.defaults.timeout = 8000;
+
+// 全局拦截器
+axios.interceptors.request.use(function(response) {
+  // 获取返回值
+  let res = response.data;
+  if (res.status == 0) {
+    return res.data;
+    // 规范状态码
+  } else if (res.status == 100) {
+    window.location.href = "/#/login";
+  } else {
+    alert(res.msg);
+  }
+});
 
 new Vue({
   router,
---
+++ vue.config.js
@@ -0,0 +1,16 @@
+// 代理
+module.exports = {
+  devServer: {
+    host: "localhost",
+    port: 8080,
+    proxy: {
+      "/api": {
+        target: "https://www.baidu.com",
+        changeOrigin: true,
+        pathRewrite: {
+          "/api": "",
+        },
+      },
+    },
+  },
+};
